125a9ba1d3889fcd05bfee8993b778144b912fa0
alhoff <andrew.hoff14@yahoo.com>
2020-09-22 23:17:09 -0700

    added result html and basic ajax call

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$(docment).ready(function() {
+$(document).ready(function() {
+    let apiKey = "9cca916a5351b86364c95b186a6fac9d"
+
+    $("#searchBtn").on("click", function() { // When search button is clicked:
+        let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + apiKey;
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(res) {
+            console.log(res)
+        });
+    })
 
 });
