f01ae33cb65d10d08ac144bb4b083c73a3e6d959
alhoff <andrew.hoff14@yahoo.com>
2020-09-23 02:16:06 -0700

    all 5 forcast cards fully functional

--- script.js
+++ script.js
@@ -40,7 +40,8 @@ $(document).ready(function () {
             let queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
             console.log(res)
-            $("#cityName").text(res.city.name + " (" +  moment().format('MMMM Do YYYY') + ")");
+            $("#cityName").html(res.city.name + " (" +  moment().format('MMMM Do YYYY') + ")");
+            $("#weather").attr("src", "http://openweathermap.org/img/wn/" + res.list[0].weather[0].icon + "@2x.png");
             $("#temp").text("Temperature: " + ((res.list[0].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "F");
             $("#humidity").text("Humidity: " + res.list[0].main.humidity + "%");
             $("#windSpeed").text("Wind speed: " + res.list[0].wind.speed + "MPH")
@@ -50,6 +51,7 @@ $(document).ready(function () {
             }).then(function (res2) {
                 $("#uvIndex").text("UV Index: " + res2.value);
             });
+            getForcast(res); // Sets forcast display
         });
     }
 
@@ -104,5 +106,27 @@ $(document).ready(function () {
             let inputCity = localStorage.getItem("History3");
             getData(inputCity);
         }
+    }
+    function getForcast(res) { // Here comes the DRYest code you've ever seen :D
+        $("#forcastDate1").text(moment().add(1, 'days').format('MMMM Do YYYY'));
+        $("#forcastDate2").text(moment().add(2, 'days').format('MMMM Do YYYY'));
+        $("#forcastDate3").text(moment().add(3, 'days').format('MMMM Do YYYY'));
+        $("#forcastDate4").text(moment().add(4, 'days').format('MMMM Do YYYY'));
+        $("#forcastDate5").text(moment().add(5, 'days').format('MMMM Do YYYY'));
+        $("#forcastWeather1").attr("src", "http://openweathermap.org/img/wn/" + res.list[1].weather[0].icon + "@2x.png");
+        $("#forcastWeather2").attr("src", "http://openweathermap.org/img/wn/" + res.list[2].weather[0].icon + "@2x.png");
+        $("#forcastWeather3").attr("src", "http://openweathermap.org/img/wn/" + res.list[3].weather[0].icon + "@2x.png");
+        $("#forcastWeather4").attr("src", "http://openweathermap.org/img/wn/" + res.list[4].weather[0].icon + "@2x.png");
+        $("#forcastWeather5").attr("src", "http://openweathermap.org/img/wn/" + res.list[5].weather[0].icon + "@2x.png");
+        $("#forcastTemp1").text("Temp: " + ((res.list[1].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "F");
+        $("#forcastTemp2").text("Temp: " + ((res.list[2].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "F");
+        $("#forcastTemp3").text("Temp: " + ((res.list[3].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "F");
+        $("#forcastTemp4").text("Temp: " + ((res.list[4].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "F");
+        $("#forcastTemp5").text("Temp: " + ((res.list[5].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "F");
+        $("#forcastHumidity1").text("Humidity: " + res.list[1].main.humidity + "%");
+        $("#forcastHumidity2").text("Humidity: " + res.list[2].main.humidity + "%");
+        $("#forcastHumidity3").text("Humidity: " + res.list[3].main.humidity + "%");
+        $("#forcastHumidity4").text("Humidity: " + res.list[4].main.humidity + "%");
+        $("#forcastHumidity5").text("Humidity: " + res.list[5].main.humidity + "%");
     }
 });
